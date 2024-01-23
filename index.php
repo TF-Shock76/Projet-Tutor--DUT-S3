f85fb07b43495eb5496ad88ee487a80217af08b5
Thibault Fouchet <thibault.fouchet@etu.univ-lehavre.fr>
2024-01-23 19:03:44 +0100

    rearrangement

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once "__inc.php";
-
-if (isset($_SESSION['login']))
-    include "accueil.inc.php";
-else
-    include "login.inc.php";
--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-header("Content-Type: text/plain");
-
-var_dump($_POST);
-
-var_dump($_FILES);
